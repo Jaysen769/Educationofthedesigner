86d7fec45b6e641aa50b42217ae179281558fe11
Jaysen Henderson <jaysenhenderson@ymail.com>
2016-03-01 16:31:19 -0500

    initial commit

---
+++ surveydata.php
@@ -0,0 +1,53 @@
+<?php
+
+$servername = 'localhost'; 
+$dbusername = 'jaysenhenderson'; 
+$dbpassword = 'Quickdraw42'; 
+$dbname = 'eotdsurvey';
+
+$con = new mysqli ($servername, $dbusername, $dbpassword, $dbname) or die("unable to connect"); 
+mysql_select_db('eotdsurvey', $con); 
+
+echo"great work"
+
+if ($con->query<$sql) === TRUE) {
+	echo "IT FUCKING WORKS.";
+}
+else{
+	echo "didnt workkkkkk";
+}
+$_POST['BI1']
+$_POST['BI2']
+$_POST['BI3']
+$_POST['BI4']
+$_POST['BI5']
+$_POST['BI6']
+$_POST['BI7']
+$_POST['BI8']
+$_POST['BI9']
+$_POST['BI10']
+$_POST['BI11']
+$_POST['BI12']
+$_POST['BI13']
+$_POST['BI14']
+$_POST['BI15']
+
+$sql = "INSERT INTO Survey1(BI1)"
+$sql = "INSERT INTO Survey1(BI2)"
+$sql = "INSERT INTO Survey1(BI3)"
+$sql = "INSERT INTO Survey1(BI4)"
+$sql = "INSERT INTO Survey1(BI5)"
+$sql = "INSERT INTO Survey1(BI6)"
+$sql = "INSERT INTO Survey1(BI7)"
+$sql = "INSERT INTO Survey1(BI8)"
+$sql = "INSERT INTO Survey1(BI9)"
+$sql = "INSERT INTO Survey1(BI10)"
+$sql = "INSERT INTO Survey1(BI11)"
+$sql = "INSERT INTO Survey1(BI12)"
+$sql = "INSERT INTO Survey1(BI13)"
+$sql = "INSERT INTO Survey1(BI14)"
+$sql = "INSERT INTO Survey1(BI15)"
+
+$conn->close();
+
+ ?>
